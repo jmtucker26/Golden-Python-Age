5fb642edc6e8e40844d2140552669884ac72fd9d
EC2 Default User <ec2-user@ip-172-31-45-97.ec2.internal>
2022-11-29 21:29:18 +0000

    redo commit week 13 Python

---
+++ wk13redo.py
@@ -0,0 +1,30 @@
+# #!/bin/python3
+
+
+
+import random
+import string
+
+
+
+def name_generator(department, number_instances):
+    dep = department.lower()
+    num = int(number_instances)
+    
+    if dep in ['Sales', 'Accounting', 'Management']:
+        name_list = []
+        for n in range(0, num):
+            rand_list = random.choices(string.ascii_letters+string.digits, k=10)
+            name=dep+"-"+''.join(rand_list)
+            name_list.append(name)
+        return(name_list)
+    else:
+        return("Your department should not use this name generator as the departments currnet supported are: sales, accounting, and management")
+    
+dep_input = input("What is your department? ")
+num_input = input("How many instances do you need named? ")
+
+
+
+result = name_generator(dep_input, num_input)
+print(result)
